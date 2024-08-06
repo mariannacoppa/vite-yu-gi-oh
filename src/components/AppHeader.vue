2a51fe23b0bb1e2d1dade1be912c8c7796f37c49
mariannacoppa <densimetra@gmail.com>
2024-08-06 14:03:56 +0200

    [FEAT] created scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <header>
+        Yu-Gi-Oh
+    </header>
+</template>
+<style lang="scss">
+    header {
+        background-color: black;
+        color: white;
+        padding: 20px 0;
+    }
+</style>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,13 +1,13 @@
 <script>
 export default {
-  
+    
 }
 </script>
 <template lang="">
-  <div>
-    
-  </div>
+    <div>
+        
+    </div>
 </template>
 <style lang="">
-  
+    
 </style>
