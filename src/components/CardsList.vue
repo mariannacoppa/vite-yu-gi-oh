130e95960ba6e7c860b9a6c5421077c8d1a8cf8a
mariannacoppa <densimetra@gmail.com>
2024-08-06 15:21:26 +0200

    [UPD] added cards

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -21,9 +21,36 @@ export default {
 </script>
 <template>
     <div>
-        ciao
+        <div class="container">
+            <div class="row-list">
+                <div class="col-20" v-for="card in store.cardsList" :key="card.id">
+                    <div class="card">
+                        <img :src="card.card_images[0].image_url" alt="">
+                        <div class="card-body">
+                            <h6>
+                                {{ card.name }}
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+    .row-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    .col-20 {
+        width: calc(100% / 5);
+    }
+    .card {
+        background-color: burlywood;
+        margin: 10px;
+        img {
+            max-width: 100%;
+        }
+    }
 </style>
